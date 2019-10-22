32e13ae893eedf396ad4f624f10c4801b3b154a2
karlmarx <5042021062karlmarx@gmail.com>
2019-10-22 10:30:07 -0400

    dtos and Repos done

---
+++ src/main/java/com/srmsolutions/repos/JobRoleRepository.java
@@ -0,0 +1,7 @@
+package com.srmsolutions.repos;
+
+import com.srmsolutions.entities.JobRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRoleRepository extends JpaRepository<JobRole, Integer> {
+}
