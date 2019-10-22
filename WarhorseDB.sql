80aebaa9f8880364e2a6b46317e8fc37e04320d9
Eric Riddle <riddle.e.w@gmail.com>
2019-10-22 11:12:57 -0400

    sql revisions

--- WarhorseDB.sql
+++ WarhorseDB.sql
@@ -11,47 +11,44 @@ description varchar(140) not null
 create table job_role (
 job_role_id int primary key auto_increment,
 name varchar(30) not null,
-description varchar(140) not null,
+description varchar(140),
 department_name varchar(30)
 );
 
 create table category (
 category_id int primary key auto_increment,
-name varchar(30) not null,
-description varchar(140) not null
+name varchar(30),
+description varchar(140) 
 );
 
 create table event (
 event_id int primary key auto_increment,
-name varchar(30) not null,
-category_id int not null,
+name varchar(30),
+category_id int,
 foreign key (category_id)
 references category(category_id),
-start_time datetime not null,
-end_time datetime not null,
-description varchar(140) not null,
-location varchar(140) not null
+start_time datetime,
+end_time datetime,
+description varchar(140),
+location varchar(140)
 );
 
 CREATE TABLE Employee (
 	employee_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
     user_role_id INT NOT NULL,
     job_role_id INT NOT NULL,
-    email VARCHAR(320) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    emergency_contact_name VARCHAR(100) NOT NULL,
-    emergency_contact_number CHAR(10) NOT NULL,
-    street VARCHAR(100) NOT NULL,
-    city VARCHAR(100)NOT NULL,
-    state CHAR(2) NOT NULL,
-    zip CHAR(5) NOT NULL,
-    hire_date DATE NOT NULL,
+    email VARCHAR(320),
+    password VARCHAR(20),
+    emergency_contact_name VARCHAR(100),
+    emergency_contact_number CHAR(10),
+    address VARCHAR(500),
+    hire_date DATE,
     termination_date DATE,
-    onsite_clearance VARCHAR(10) NOT NULL,
-    hours_of_pto TINYINT NOT NULL,
-    SSN CHAR(9) NOT NULL,
+    onsite_clearance VARCHAR(10) NOT NULL DEFAULT "LOW",
+    hours_of_pto TINYINT,
+    SSN CHAR(9),
     manager_id INT,
     FOREIGN KEY (job_role_id)
         REFERENCES job_role (job_role_id),
@@ -73,7 +70,7 @@ CREATE TABLE Employee_of_Week (
 	employee_of_week_id INT PRIMARY KEY AUTO_INCREMENT,
     employee_id INT NOT NULL,
     week_of_date DATE NOT NULL,
-    description VARCHAR(500) NOT NULL,
+    description VARCHAR(500),
     FOREIGN KEY (employee_id)
         REFERENCES Employee (employee_id)
 	);
